97bac9315c42b950450b9e54a38affe0b70da126
robmyers82 <robert_myers_@hotmail.com>
2018-03-26 23:05:25 -0300

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+class FreeAgentTracker extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                <p>Start your code here!</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<FreeAgentTracker />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+module.exports = {
+    // this is where we'll configure Webpack!
+};
